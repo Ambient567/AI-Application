cac5d65e9df2222bc74cdc9a406b92cc104c0ad8
Precious Nhlapho <pnhlapho59@gmail.com>
2026-03-02 21:53:59 +0200

    Utilizing systems messages

---
+++ Chat roles/system_msgs.py
@@ -0,0 +1,28 @@
+import os 
+from dotenv import load_dotenv
+from openai import OpenAI
+
+load_dotenv()
+
+api_key = os.getenv("OPENAI_API_KEY")
+
+if not api_key:
+    raise ValueError("API key not found")
+
+
+client = OpenAI(api_key=api_key)
+
+# Create a request to the Chat Completions endpoint
+response = client.chat.completions.create(
+  model="gpt-4o-mini",
+  max_completion_tokens=150,
+  messages=[
+    {"role":"system",
+     "content": "You are a study planning assistant that creates plans for learning new skills."},
+    {"role": "user",
+     "content": "I want to learn to speak Dutch."}
+  ]
+)
+
+# Extract the assistant's text response
+print(response.choices[0].message.content)
