3d340d4b8e5cb93d672e15a7aafa977e5f5f3fcd
Precious <pnhlapho59@gmail.com>
2026-05-04 21:31:20 +0200

    Add Multi-Turn Conversations second example

---
+++ Chat roles/multi_turn_v2.py
@@ -0,0 +1,36 @@
+import os 
+from dotenv import load_dotenv
+from openai import OpenAI
+
+load_dotenv()
+
+api_key = os.getenv("OPENAI_API_KEY")
+
+if not api_key:
+    raise ValueError("API key not found")
+
+client = OpenAI(api_key=api_key)
+
+
+messages = [{"role": "system", "content": "You are a helpful math tutor that speaks concisely."}]
+user_msgs = ["Explain what pi is.", "Summarize this in two bullet points."]
+
+# Loop over the user questions
+for q in user_msgs:
+    print("User: ", q)
+    
+    # Create a dictionary for the user message from q and append to messages
+    user_dict = {"role": "user", "content": q}
+    messages.append(user_dict)
+    
+    # Create the API request
+    response = client.chat.completions.create(
+        model="gpt-4o-mini",
+        messages=messages,
+        max_completion_tokens=100
+    )
+    
+    # Append the assistant's message to messages
+    assistant_dict = {"role": "assistant", "content": response.choices[0].message.content}
+    messages.append(assistant_dict)
+    print("Assistant: ", response.choices[0].message.content, "\n")
